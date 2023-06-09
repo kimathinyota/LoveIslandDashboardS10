cebec81af23a6844d200ce83fd1898b02a547a99
kimathinyyota <kn3g17@soton.ac.uk>
2023-06-09 12:28:27 +0100

    updating co-mentions for version 5 altair

--- LoveIslandDashboard/Version4Altair/CoMentions.py
+++ LoveIslandDashboard/Version4Altair/CoMentions.py
@@ -50,10 +50,10 @@ def create_interactive_influence_graph(filtered_edge_df_by_day, islander_to_cent
     chart_edges = chart.layer[0]
     chart_nodes = chart.layer[1]
 
-    pick_name = alt.selection_point(fields=['name'],  value=[{'name': most_central_islander}], empty='none')
+    pick_name = alt.selection_single(fields=['name'],  init={'name': most_central_islander}, empty='none')
     chart_nodes = chart_nodes.encode(
         opacity = alt.condition(pick_name, alt.value(1.0), alt.value(0.15))
-    ).add_params(
+    ).add_selection(
         pick_name
     )
 
